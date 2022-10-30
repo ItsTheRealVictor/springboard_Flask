a3855b1279e2b7dfce371a4cd9d61ce70771302b
Victor Delgado <valexdelgado@gmail.com>
2022-10-30 14:55:50 -0700

    starting on the flask exercises

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,19 @@
 # Put your app in here.
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome_page():
+    return 'welcome'
+
+@app.route('/welcome/home')
+def welcome_home_page():
+    return 'welcome home'
+
+@app.route('/welcome/back')
+def welcome_back_page():
+    return 'welcome back'
+
+if __name__ == '__main__':
+    app.run(debug=True)
