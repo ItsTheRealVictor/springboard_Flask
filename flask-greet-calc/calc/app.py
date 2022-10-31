073007b9267a46e8cce6fac9b3617355191d0b23
Victor Delgado <valexdelgado@gmail.com>
2022-10-30 22:16:39 -0700

    finished with intro to flask exercises

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -7,6 +7,46 @@ app = Flask(__name__)
 def main_page():
     return 'the main page for the Flask calculator exercise'
 
+@app.route('/add')
+def addition():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(add(a, b))
+
+@app.route('/sub')
+def subtraction():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(sub(a, b))
+
+@app.route('/mult')
+def multiplication():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(mult(a, b))
+
+    
+@app.route('/div')
+def division():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(div(a, b))
+
+
+operations = {
+    'add': add,
+    'sub': sub,
+    'div': div,
+    'mult': mult
+}
+
+@app.route('/math/<operation>')
+def do_math(operation):
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return str(operations[operation](a,b))
+
 
 
 
